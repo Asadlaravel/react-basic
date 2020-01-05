58a94cc6df12e6b83db756343899b20af0fb7da8
Asad Ullah <asaddir5566@gmail.com>
2020-01-05 13:10:05 +0500

    Clock App

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
-ReactDOM.render(<h1>Hellow</h1>, document.getElementById("root"));
+setInterval(function() {
+  ReactDOM.render(
+    <h1>{new Date().toLocaleTimeString()}</h1>,
+    document.getElementById("root")
+  );
+}, 1000);
